f6898ba919b1d797c1951cd03f9a5a061220bd2a
sohaibnoor <iamsohaibnoor@gmail.com>
2019-09-03 00:12:33 +0500

    Updates

---
+++ precache-manifest.a044d702f88f8f47ddb2299dfc538966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7489d063d0a7b4ddbcac72abc8f6ccac",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "42fcef153bc0e3d776c3",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "347da13ec906671f2a26",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "42fcef153bc0e3d776c3",
+    "url": "/contactmanager/static/js/2.0eab3603.chunk.js"
+  },
+  {
+    "revision": "347da13ec906671f2a26",
+    "url": "/contactmanager/static/js/main.14f40370.chunk.js"
+  },
+  {
+    "revision": "440bac45752b04fd4578",
+    "url": "/contactmanager/static/js/runtime~main.43891527.js"
+  }
+]);
